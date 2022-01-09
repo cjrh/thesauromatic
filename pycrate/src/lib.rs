40bb1cdd63ab81a578c041fee000b3e52f7fc57e
Caleb Hattingh <caleb.hattingh@gmail.com>
2022-01-09 18:27:33 +1000

    Remove the "sum as string" demo code

--- pycrate/src/lib.rs
+++ pycrate/src/lib.rs
@@ -1,11 +1,5 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
 #[pyfunction]
 fn synonyms(word: &str) -> PyResult<Vec<String>> {
     let out = thesauromatic::lookup(word);
@@ -17,7 +11,6 @@ fn synonyms(word: &str) -> PyResult<Vec<String>> {
 /// import the module.
 #[pymodule]
 fn thesauromatic_py(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(synonyms, m)?)?;
 
     Ok(())
