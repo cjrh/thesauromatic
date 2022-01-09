9904d7875089369b27c0e7b9ec6a54bb65839cf0
Caleb Hattingh <caleb.hattingh@gmail.com>
2022-01-09 18:25:54 +1000

    Added an internal crate to build the python package

---
+++ src/python_module.rs
@@ -0,0 +1,25 @@
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+pub fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pyfunction]
+pub fn synonyms(word: &str) -> PyResult<Vec<String>> {
+    let out = thesauromatic::lookup(word);
+    Ok(out)
+}
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+pub fn thespy(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(synonyms, m)?)?;
+
+    Ok(())
+}
+
